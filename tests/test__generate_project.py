09be8a236e581606b9623ade77607730e2f4faba
Eric Riddoch <eric.russia97@gmail.com>
2023-08-21 12:08:53 -0600

    feat: wrote initial project_dir fixture in same file as test

--- tests/test__generate_project.py
+++ tests/test__generate_project.py
@@ -1,18 +1,29 @@
 import json
+import shutil
 import subprocess
+from copy import deepcopy
 from pathlib import Path
+from typing import Dict
+
+import pytest
 
 THIS_DIR = Path(__file__).parent
 PROJECT_DIR = (THIS_DIR / "../").resolve()
 
-def test__can_generate_project():
-    """
-    
-    execute: `cookiecutter <template directory> ...`
-    """
-    cookiecutter_config = {
-        "default_context": {"repo_name": "test-repo"}
+
+@pytest.fixture(scope="function")
+def project_dir() -> Path:
+    template_values = {
+        "repo_name": "test-repo",
     }
+    generated_repo_dir: Path = generate_project(template_values=template_values)
+    yield generated_repo_dir
+    shutil.rmtree(path=generated_repo_dir)
+
+
+def generate_project(template_values: Dict[str, str]):
+    template_values: Dict[str, str] = deepcopy(template_values)
+    cookiecutter_config = {"default_context": template_values}
     cookiecutter_config_fpath = PROJECT_DIR / "tests/cookiecutter.json"
     cookiecutter_config_fpath.write_text(json.dumps(cookiecutter_config))
 
@@ -24,10 +35,18 @@ def test__can_generate_project():
         "--no-input",
         "--config-file",
         str(cookiecutter_config_fpath),
-        "--verbose"
+        "--verbose",
     ]
     print("COMMAND:", " ".join(cmd))
     subprocess.run(cmd, check=True)
 
-    generated_project_dir = PROJECT_DIR / "sample" / cookiecutter_config["default_context"]["repo_name"]
-    assert generated_project_dir.exists()
+    generated_repo_dir = PROJECT_DIR / "sample" / template_values["repo_name"]
+    return generated_repo_dir
+
+
+def test__can_generate_project(project_dir: Path):
+    """
+
+    execute: `cookiecutter <template directory> ...`
+    """
+    assert project_dir.exists()
